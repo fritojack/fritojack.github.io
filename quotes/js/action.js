2c3a918abcb42a48758330db78cc91769f8bd65a
Sam C <fritojack215@gmail.com>
2017-06-12 08:54:00 -0500

    BUG Aligned Buttons

--- quotes/js/action.js
+++ quotes/js/action.js
@@ -1,4 +1,6 @@
 $(document).ready(function(val){
+    var quote = "Youhoo";
+
     $(".quote-do").on("click",function(){
 	var html = ""
 	$.ajax({
@@ -6,11 +8,20 @@ $(document).ready(function(val){
 	    dataType: 'jsonp',
 	    data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
 	    success: function(data){
+		quote = data.quoteText + " " + data.quoteAuthor;
 		html = data.quoteText + "<br>" + data.quoteAuthor;
-	$(".quote-go-here").html(html);
+		$(".quote-go-here").html(html);
 	    }
 	});
 
+    });
+    
+    $(".twitter").on("click",function(){
+	var textToTweet = quote;
+	var tweetLink = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(textToTweet);
+	$(".twitter").attr('href',tweetLink).click();
 
     });
+    
+
 });
